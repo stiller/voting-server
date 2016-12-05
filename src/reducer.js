2b237d52b68d680501b2245374257ef334cf8b01
Joachim Nolten <joachim@nolten.org>
2016-12-05 16:37:48 +0100

    Reducer composition.

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,9 @@ export default function reducer(state = INITIAL_STATE, action) {
   switch(action.type) {
     case 'SET_ENTRIES': return setEntries(state, action.entries);
     case 'NEXT': return next(state);
-    case 'VOTE': return vote(state, action.entry);
+    case 'VOTE':
+      return state.update('vote',
+                          voteState => vote(voteState, action.entry));
   }
   return state;
 }
